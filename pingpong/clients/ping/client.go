9ae5f9a13b47018b54c2be9a3013658715fc3f43
Bobinette <xavier.chassin@live.fr>
2016-05-22 23:51:05 +0200

    Update(pingpong): connect multiple clients and play

--- pingpong/clients/ping/client.go
+++ pingpong/clients/ping/client.go
@@ -1,18 +1,28 @@
 package main
 
 import (
+	"log"
+	"math/rand"
+	"time"
+
+	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 
 	"github.com/bobinette/deadpool/pingpong/protos"
 )
 
+const name = "Ping"
+
 type Client interface {
 	Connect() error
+	Disconnect() error
 }
 
 type client struct {
 	id  int32
 	ppc protos.PingPongClient
+
+	stream protos.PingPong_ConnectClient
 }
 
 func NewClient(cc *grpc.ClientConn) Client {
@@ -22,5 +32,80 @@ func NewClient(cc *grpc.ClientConn) Client {
 }
 
 func (c *client) Connect() error {
+	stream, err := c.ppc.Connect(context.Background(), &protos.ConnectRequest{Name: name})
+	if err != nil {
+		return err
+	}
+	log.Println("Successfully contacted server")
+
+	// Wait for id
+	n, err := stream.Recv()
+	if err != nil {
+		return err
+	}
+	c.id = n.GetIdMessage().Id
+	log.Printf("Got id %d", c.id)
+
+	go c.monitor(stream)
+
+	go func() {
+		src := rand.NewSource(time.Now().UnixNano())
+		gen := rand.New(src)
+		for {
+			time.Sleep(time.Duration(gen.Intn(5)) * time.Second)
+			err := c.play()
+			if err != nil {
+				log.Printf("Error while requesting for game status: %v", err)
+				return
+			}
+		}
+	}()
+
+	return nil
+}
+
+func (c *client) Disconnect() error {
+	// Close stream
+	err := c.stream.CloseSend()
+	if err != nil {
+		log.Printf("Error closing stream: %v", err)
+		return err
+	}
+	log.Println("Stream closed")
+	return nil
+}
+
+func (c *client) monitor(stream protos.PingPong_ConnectClient) {
+	c.stream = stream
+
+	// Randomly request for game status
+	src := rand.NewSource(time.Now().UnixNano())
+	gen := rand.New(src)
+	for {
+		time.Sleep(time.Duration(gen.Intn(20)) * time.Second)
+		req := protos.IdMessage{Id: c.id}
+		s, err := c.ppc.GetGameStatus(context.Background(), &req)
+		if err != nil {
+			log.Printf("Error while requesting for game status: %v", err)
+			return
+		}
+		log.Println(s.Pingpong)
+	}
+}
+
+func (c *client) play() error {
+	req := protos.PlayRequest{
+		Id: c.id,
+	}
+	res, err := c.ppc.Play(context.Background(), &req)
+	if err != nil {
+		return err
+	}
+
+	if !res.Accepted {
+		log.Println("Play not accepted")
+	} else {
+		log.Println("Play was accepted")
+	}
 	return nil
 }
--- pingpong/server/server.go
+++ pingpong/server/server.go
@@ -1,13 +1,105 @@
 package server
 
 import (
+	"log"
+	"math/rand"
+	"time"
+
+	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+
+	"github.com/bobinette/deadpool/pingpong/protos"
 )
 
 type server struct {
+	clients map[int32]string
+	lastId  int32
+
+	notifier      Notifier
+	game          Game
+	currentPlayer int32
 }
 
 func NewServer() *grpc.Server {
 	s := grpc.NewServer()
+
+	pps := server{
+		clients: make(map[int32]string),
+		lastId:  0,
+
+		notifier:      NewNotifier(),
+		game:          NewGame(),
+		currentPlayer: -1,
+	}
+	protos.RegisterPingPongServer(s, &pps)
 	return s
+}
+
+func (s *server) Connect(in *protos.ConnectRequest, stream protos.PingPong_ConnectServer) error {
+	s.lastId += 1
+
+	// Remember client
+	id := s.lastId
+	s.clients[id] = in.Name
+	log.Printf("New client: %s (%d)", in.Name, id)
+
+	// Register client
+	s.notifier.Register(id, stream)
+
+	s.currentPlayer = s.nextPlayer()
+
+	// Send back it's id to the player
+	n := s.idNotification(id)
+	if err := s.notifier.Notify(id, n); err != nil {
+		log.Printf("Error notifying %s (%d): %v", in.Name, id, err)
+		return err
+	}
+
+	return nil
+}
+
+func (s *server) GetGameStatus(ctx context.Context, in *protos.IdMessage) (*protos.GameStatus, error) {
+	status := s.game.Status()
+	log.Printf("%d requested the game status", in.Id)
+	return status, nil
+}
+
+func (s *server) Play(ctx context.Context, in *protos.PlayRequest) (*protos.PlayReply, error) {
+	id := in.Id
+
+	if id != s.currentPlayer {
+		return &protos.PlayReply{Accepted: false}, nil
+	}
+
+	rep, err := s.game.Play(id)
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("%d played", in.Id)
+	s.currentPlayer = s.nextPlayer()
+	return rep, nil
+}
+
+// ---- Notification crafting
+func (s *server) idNotification(id int32) *protos.Notification {
+	idMessage := &protos.IdMessage{
+		Id: id,
+	}
+	return &protos.Notification{
+		Body: &protos.Notification_IdMessage{
+			IdMessage: idMessage,
+		},
+	}
+}
+
+// ---- Turns
+func (s *server) nextPlayer() int32 {
+	var ids []int32
+	for k := range s.clients {
+		ids = append(ids, k)
+	}
+
+	src := rand.NewSource(time.Now().UnixNano())
+	gen := rand.New(src)
+	return ids[gen.Intn(len(ids))]
 }
