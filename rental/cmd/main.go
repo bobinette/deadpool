7318d9eb5e12f6daeaee10f25dc11125391061b2
Bobinette <xavier.chassin@live.fr>
2016-10-27 16:07:57 +0200

    Choose player dynamically

--- rental/cmd/main.go
+++ rental/cmd/main.go
@@ -2,18 +2,33 @@ package main
 
 import (
 	"flag"
+	"log"
 	"math/rand"
 	"time"
 
 	"github.com/bobinette/deadpool/rental"
 )
 
+type Player interface {
+	Play(rental.Game)
+}
+
 func main() {
-	// r := flag.Bool("reset", false, "set to true to reset the cache file")
+	playerName := flag.String("player", "mc", "player to use")
 	flag.Parse()
 
 	game := rental.NewGame()
-	p := rental.MC{Rand: rand.New(rand.NewSource(time.Now().UnixNano()))}
+	var p Player
+
+	if *playerName == "dp" {
+		p = &rental.DP{}
+		log.Println("Using Dynamic Programming player")
+	} else if *playerName == "mc" {
+		p = &rental.MC{Rand: rand.New(rand.NewSource(time.Now().UnixNano()))}
+		log.Println("Using Monte Carlo player")
+	} else {
+		log.Fatalln("Unknown player: ", *playerName)
+	}
 
 	p.Play(game)
 }
